6cb178e8f74cf0c25b5c59a9868ef9bc89561486
Brecht Machiels <brecht@mos6581.org>
2018-04-30 11:04:51 +0200

    Initial Commit

---
+++ GoogleKeepMenu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GoogleKeepMenu
+//
+//  Created by Brecht Machiels on 30/04/2018.
+//  Copyright © 2018 MOS6581. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
